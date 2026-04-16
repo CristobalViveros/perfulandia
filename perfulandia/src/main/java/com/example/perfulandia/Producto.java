db26ebfb0609cf7b94d24134d7aacd647be7e996
Axel-juris <ax.juris@duocuc.cl>
2026-04-16 12:30:01 -0400

    Update Producto.java

--- perfulandia/src/main/java/com/example/perfulandia/Producto.java
+++ perfulandia/src/main/java/com/example/perfulandia/Producto.java
@@ -1,7 +1,15 @@
 package com.example.perfulandia;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Valid
+
 public class Producto {
     private int id;
     private String nombre;
+    private int stock_actual; 
+    private int precio;
+
 
 }
