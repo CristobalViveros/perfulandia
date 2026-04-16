3ae520c9be34137b85fab05f8351ca2737ed9e96
Cristobal Viveros <cris.viveros@duocuc.cl>
2026-04-16 12:14:28 -0400

    Proyecto Principal

---
+++ perfulandia/src/main/java/com/example/perfulandia/model/Usuario.java
@@ -0,0 +1,18 @@
+package com.example.perfulandia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Valid
+@Table
+public class Usuario {
+    private int id;
+}
